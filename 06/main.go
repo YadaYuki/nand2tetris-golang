72464779b7a453ed8c8fe90b44c325d5e20a8dec
YadaYuki <yada.yuki@fuji.waseda.jp>
2021-02-14 17:29:52 +0900

    add symbol table module

--- 06/main.go
+++ 06/main.go
@@ -9,6 +9,7 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"strings"
 )
 
 func main() {
@@ -24,9 +25,13 @@ func main() {
 
 	for fileScanner.Scan() {
 		s := fileScanner.Text()
-		if len(s) > 0 {
-			commandType, _ := parser.GetCommandType(s)
 
+		if isComment := strings.Index(s, "//"); len(s) > 0 && isComment == -1 {
+			commandType, err := parser.GetCommandType(s)
+			if err != nil {
+				log.Fetal(err)
+
+			}
 			binaryCode := ""
 			if commandType == parser.CCommand {
 				dest, err := parser.GetDest(s)
@@ -55,13 +60,14 @@ func main() {
 				}
 				binaryCode = "111" + compBinary + destBinary + jumpBinary
 			}
-			if commandType == parser.LCommand {
-			}
 			if commandType == parser.ACommand {
-				symbolStr, _ := parser.GetSymbol(s)
-				if symbol, err := strconv.Atoi(symbolStr); err == nil {
+				symbolStr, err := parser.GetSymbol(s)
+				symbol, err := strconv.Atoi(symbolStr)
+				if err == nil {
 					binaryCode = "0" + util.Fill(strconv.FormatInt(int64(symbol), 2), "0", 15)
 				}
+			}
+			if commandType == parser.LCommand {
 			}
 			// TODO:implement Write file
 			fmt.Println(binaryCode)
