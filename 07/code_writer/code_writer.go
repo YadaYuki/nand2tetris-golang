07ea5ca6e60027b166d9d63c5f0872224863441c
YadaYuki <yada.yuki@fuji.waseda.jp>
2021-02-20 18:57:11 +0900

    add get arithmetic

--- 07/code_writer/code_writer.go
+++ 07/code_writer/code_writer.go
@@ -4,6 +4,7 @@ import (
 	"VMtranslator/parser"
 	"errors"
 	"strconv"
+	"strings"
 )
 
 // GetPushPop get
@@ -18,4 +19,30 @@ func GetPushPop(commandType parser.CommandType, segment string, index int) (asse
 		return "", nil
 	}
 	return "", errors.New("Invalid Command Type")
+}
+
+// GetArithmetic returns assembly for arithmetic command
+func GetArithmetic(commandStr string) (assembly string, err error) {
+	s := strings.TrimSpace(commandStr)
+	// TODO: to switch
+	if s == "add" {
+		return "@SP\n" + "A=M\n" + "D=M\n" + "A=A-1\n" + "D=D+M\n" + "M=D\n" + "@SP\n" + "M=M-1\n", nil
+	}
+	if s == "sub" {
+		return "@SP\n" + "A=M\n" + "D=M\n" + "A=A-1\n" + "M=M-D\n" + "@SP\n" + "M=M-1\n", nil
+	}
+	if s == "neg" {
+		return "@SP\n" + "A=M\n" + "D=M\n" + "M=-D\n", nil
+	}
+	if s == "and" {
+		return "@SP\n" + "A=M\n" + "D=M\n" + "A=A-1\n" + "D=D&M\n" + "M=D\n" + "@SP\n" + "M=M-1\n", nil
+	}
+	if s == "or" {
+		return "@SP\n" + "A=M\n" + "D=M\n" + "A=A-1\n" + "D=D|M\n" + "M=D\n" + "@SP\n" + "M=M-1\n", nil
+	}
+	if s == "not" {
+		return "@SP\n" + "A=M\n" + "D=M\n" + "M=!D\n", nil
+	}
+
+	return "", errors.New("invalid arithmetic command")
 }
