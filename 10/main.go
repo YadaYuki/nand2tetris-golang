580c954c72dfe3151292add28b1701d68f77df50
YadaYuki <yada.yuki@fuji.waseda.jp>
2021-03-06 19:37:05 +0900

    add JackTokenizer Initializer

--- 10/main.go
+++ 10/main.go
@@ -1,24 +1,22 @@
 package main
 
 import (
-	"bufio"
-	"flag"
-	"fmt"
-	"log"
-	"os"
+	"jack_compiler/jack_tokenizer"
 )
 
 func main() {
-	flag.Parse()
-	filename := flag.Args()[0]
-	fmt.Println(filename)
-	file, err := os.Open(filename)
-	if err != nil {
-		log.Fatal("%s", err)
-	}
-	fileScanner := bufio.NewScanner(file)
-	for fileScanner.Scan() {
-		s := fileScanner.Text()
-		fmt.Println(s)
-	}
+	// flag.Parse()
+	// filename := flag.Args()[0]
+	// fmt.Println(filename)
+	// file, err := os.Open(filename)
+	// if err != nil {
+	// 	log.Fatal("%s", err)
+	// }
+	// fileScanner := bufio.NewScanner(file)
+	// for fileScanner.Scan() {
+	// 	s := fileScanner.Text()
+	// 	fmt.Println(s)
+	// }
+	jt := jack_tokenizer.New("")
+	jt.HasMoreTokens()
 }
