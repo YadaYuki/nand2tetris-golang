7d92ff78db0396f675734e15cf354e9d98a61e94
YadaYuki <yada.yuki@fuji.waseda.jp>
2021-03-11 09:56:29 +0900

    Add Compilation Engine

--- 10/jack_analyzer/jack_analyzer.go
+++
@@ -1,9 +0,0 @@
-package jack_analyzer
-
-// Lexer has member necessary for parsing
-type Lexer struct {
-	input        string // code input
-	position     int
-	readPosition int
-	ch           byte
-}
