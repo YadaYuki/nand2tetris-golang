9d96791cbd359f7b8c1380b638b9b07086886918
YadaYuki <yada.yuki@fuji.waseda.jp>
2021-03-16 22:03:48 +0900

    Add Ast

--- 10/ast/ast.go
+++ 10/ast/ast.go
@@ -1,5 +1,9 @@
 package ast
 
+import (
+	"jack_compiler/token"
+)
+
 // Node is Node of AST
 type Node interface {
 	TokenLiteral() string
@@ -16,3 +20,21 @@ type Expression interface {
 	Node
 	expressionNode()
 }
+
+// LetStatement is Ast of "let"
+type LetStatement struct {
+	Statement
+	Name  *Identifier
+	Value *Expression
+}
+
+// Identifier is let variable type
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) expressionNode() {}
+
+// TokenLiteral returns literal of token
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
