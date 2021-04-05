e4415f5d369912bf1f1f3921cd3a5385afb897ec
YadaYuki <yada.yuki@fuji.waseda.jp>
2021-04-05 21:54:22 +0900

    Add AST

--- 10/ast/ast.go
+++ 10/ast/ast.go
@@ -169,3 +169,13 @@ func (ie *InfixExpression) String() string{
 }
 
 
+
+type Boolean struct {
+	Token token.Token
+	Value bool
+}
+
+func (b *Boolean) expressionNode() {}
+func (b *Boolean) TokenLiteral() string { return b.Token.Literal}
+func (b *Boolean) String() string { return b.Token.Literal}
+
