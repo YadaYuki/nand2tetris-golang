aea3b09df9a23567f5dfbce2f18800babd10b733
YadaYuki <yada.yuki@fuji.waseda.jp>
2021-05-05 20:42:32 +0900

    Fixed

--- 10/main.go
+++ 10/main.go
@@ -8,35 +8,22 @@ import (
 
 func main() {
 	jt := tokenizer.New(`
-	class Main {
-    static boolean test;   
-                           
-    function void main() {
-      var SquareGame game;
-      let game = SquareGame.new();
-      do game.run();
-      do game.dispose();
-      return;
-    }
+    class Main {
 
-    function void more() {  
-        var int i, j;       
-        var String s;
-        var Array a;
-        if (false) {
-            let s = "string constant";
-            let s = null;
-            let a[1] = a[2];
-        }
-        else {              
-            let i = i * (-j);
-            let j = j / (-2);  
-            let i = i | j;
-        }
-        return;
-    }
-}
-	`)
+   constructor Square new(int Ax, int Ay, int Asize) {
+    let x = Ax;
+    let y = Ay;
+    let size = Asize;
+    do draw;
+    return x;
+ }
+    function void main() {
+        var int length;
+        var int i, sum;
+        return a;
+      }
+    }`)
 	ce := compilationengine.New(jt)
-	fmt.Println(ce.ParseProgram())
+	fmt.Println(ce.ParseProgram().Xml())
+
 }
