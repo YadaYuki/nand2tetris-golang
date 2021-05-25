696b679062435832f2ed904c2b669e68ac7b8057
YadaYuki <yada.yuki@fuji.waseda.jp>
2021-05-25 13:10:48 +0900

    Add Symbol Table

--- 11/symboltable/symboltable.go
+++ 11/symboltable/symboltable.go
@@ -3,6 +3,20 @@ package symboltable
 type SymbolTable struct {
 }
 
+type Symbol struct {
+	Name    string
+	VarKind VarKind
+	VarType string
+	Idx     int
+}
+
+type VarKind string
+
+// const (
+// 	STATIC VarKind = "static"
+// 	FIE
+// )
+
 func New() *SymbolTable {
 	return &SymbolTable{}
 }
