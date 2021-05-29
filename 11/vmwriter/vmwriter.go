79b93954d26fc7276f9cadbe12c2a339e86c9149
YadaYuki <yada.yuki@fuji.waseda.jp>
2021-05-29 17:02:04 +0900

    Add Segment and COmmand

--- 11/vmwriter/vmwriter.go
+++ 11/vmwriter/vmwriter.go
@@ -3,6 +3,37 @@ package vmwriter
 type VMWriter struct {
 }
 
+type Command string
+
+const (
+	ADD Command = "add"
+	SUB Command = "sub"
+	NEG Command = "neg"
+	EQ  Command = "eq"
+	GT  Command = "gt"
+	LT  Command = "lt"
+	AND Command = "and"
+	OR  Command = "or"
+	NOT Command = "not"
+)
+
+type Segment string
+
+const (
+	CONST   Segment = "const"
+	ARG     Segment = "arg"
+	LOCAL   Segment = "local"
+	STATIC  Segment = "static"
+	THIS    Segment = "this"
+	THAT    Segment = "that"
+	POINTER Segment = "pointer"
+	TEMP    Segment = "temp"
+)
+
 func New() *VMWriter {
 	return &VMWriter{}
+}
+
+func (vm *VMWriter) WritePush() string {
+	return ""
 }
