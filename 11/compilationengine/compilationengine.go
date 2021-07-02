88f29c80ca493056276a2a4f3e5eb3c3cf0b9cdc
YadaYuki <yada.yuki@fuji.waseda.jp>
2021-07-02 10:53:39 +0900

    Construct Compilation Engine

--- 11/compilationengine/compilationengine.go
+++ 11/compilationengine/compilationengine.go
@@ -1,6 +1,8 @@
 package compilationengine
 
 import (
+	"errors"
+	"jack_compiler/ast"
 	"jack_compiler/parser"
 	"jack_compiler/symboltable"
 	"jack_compiler/vmwriter"
@@ -8,13 +10,34 @@ import (
 
 // CompilationEngine is struct
 type CompilationEngine struct {
-	parser *parser.Parser
-	vm     *vmwriter.VMWriter
-	st     *symboltable.SymbolTable
+	*parser.Parser
+	*vmwriter.VMWriter
+	*symboltable.SymbolTable
 }
 
 // New is initializer of compilation engine
 func New(parser *parser.Parser, vm *vmwriter.VMWriter, st *symboltable.SymbolTable) *CompilationEngine {
-	ce := &CompilationEngine{parser: parser, vm: vm, st: st}
+	ce := &CompilationEngine{Parser: parser, VMWriter: vm, SymbolTable: st}
 	return ce
+}
+
+func (ce *CompilationEngine) CompileProgram() {
+	programAst := ce.ParseProgram()
+	for _, stmtAst := range programAst.Statements {
+		ce.compileStatement(stmtAst)
+	}
+}
+
+func (ce *CompilationEngine) compileStatement(statementAst ast.Statement) error {
+	switch statementAst := statementAst.(type) {
+	case *ast.VarDecStatement:
+		return ce.compileVarDec(statementAst)
+	default:
+		return errors.New("statementAst type: %T is not valid")
+	}
+}
+
+func (ce *CompilationEngine) compileVarDec(varDecAst *ast.VarDecStatement) error {
+	ce.WriteIf("hoge")
+	return nil
 }
