3cc48c312ff2a113069f9c11fb85c5ab44634bae
YadaYuki <yada.yuki@fuji.waseda.jp>
2021-07-20 13:42:54 +0900

    Add Arithmetic Command Type

--- 07/ast/ast.go
+++ 07/ast/ast.go
@@ -1,1 +1,15 @@
 package ast
+
+type AritimeticCommandType string
+
+const (
+	ADD AritimeticCommandType = "add"
+	SUB AritimeticCommandType = "sub"
+	NEG AritimeticCommandType = "neg"
+	EQ  AritimeticCommandType = "eq"
+	GT  AritimeticCommandType = "gt"
+	LT  AritimeticCommandType = "lt"
+	AND AritimeticCommandType = "and"
+	OR  AritimeticCommandType = "or"
+	NOT AritimeticCommandType = "not"
+)
