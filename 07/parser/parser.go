8f018228cd6865917261e733c6fb7e4e6c89ab8b
YadaYuki <yada.yuki@fuji.waseda.jp>
2021-07-20 15:01:34 +0900

    Add Parser And Its Constructor

--- 07/parser/parser.go
+++ 07/parser/parser.go
@@ -1,1 +1,20 @@
 package parser
+
+import (
+	"VMtranslator/value"
+	"strings"
+)
+
+type Parser struct {
+	CurrentCommandIdx      int
+	CurrentTokenIdx        int
+	CommandStrArr          []string // ["push local 1",....]
+	CurrentCommandTokenArr []string // ["push","local","1"]
+	input                  string
+}
+
+func New(input string) *Parser {
+	CommandStrArr := strings.Split(input, value.NEW_LINE)
+	InitialCurrentCommandTokenArr := strings.Split(CommandStrArr[0], value.SPACE)
+	return &Parser{input: input, CurrentCommandIdx: 0, CurrentTokenIdx: 0, CommandStrArr: CommandStrArr, CurrentCommandTokenArr: InitialCurrentCommandTokenArr}
+}
