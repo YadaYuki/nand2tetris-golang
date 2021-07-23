48fff041a50a09b0adada3b964c86fae4d5e5bc9
YadaYuki <yada.yuki@fuji.waseda.jp>
2021-07-23 17:28:27 +0900

    Add writeAssembly

--- 07/codewriter/codewriter.go
+++ 07/codewriter/codewriter.go
@@ -4,18 +4,22 @@ import (
 	"io/ioutil"
 )
 
-type CodeWirter struct {
+type CodeWriter struct {
 	Filename string
 	Assembly []byte
 }
 
-func New(filename string) *CodeWirter {
-	return &CodeWirter{Filename: filename, Assembly: []byte{}}
+func New(filename string) *CodeWriter {
+	return &CodeWriter{Filename: filename, Assembly: []byte{}}
 }
 
-func (codeWriter *CodeWirter) Close() {
+func (codeWriter *CodeWriter) Close() {
 	err := ioutil.WriteFile(codeWriter.Filename, codeWriter.Assembly, 0644)
 	if err != nil {
 		panic(err)
 	}
+}
+
+func (codeWriter *CodeWriter) writeAssembly(assembly string) {
+	codeWriter.Assembly = append(codeWriter.Assembly, []byte(assembly)...)
 }
--- 07/codewriter/codewriter_test.go
+++ 07/codewriter/codewriter_test.go
@@ -10,12 +10,22 @@ func TestClose(t *testing.T) {
 
 	assembly := []byte("Hello,World")
 	filename := "test.asm"
-	codeWriter := &CodeWirter{
+	codeWriter := &CodeWriter{
 		Assembly: assembly, Filename: filename,
 	}
 	codeWriter.Close()
 	content, _ := ioutil.ReadFile(filename)
 	if !bytes.Equal(content, assembly) {
 		t.Fatalf("assembly should be %s. got %s", assembly, content)
+	}
+}
+
+func TestWriteAssembly(t *testing.T) {
+
+	assembly := "Hello,World"
+	codeWriter := New("test.asm")
+	codeWriter.writeAssembly(string(assembly))
+	if !bytes.Equal(codeWriter.Assembly, []byte(assembly)) {
+		t.Fatalf("assembly should be %s. got %s", assembly, codeWriter.Assembly)
 	}
 }
